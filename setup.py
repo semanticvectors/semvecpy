2b75797b969e4d7541a7ba5ad24839bd963c3f31
Dominic Widdows <dominic.widdows@grab.com>
2019-08-14 16:32:31 +0100

    Bumping developer version number to allow re-upload to test.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="semvecpy",
-    version="0.1",
+    version="0.1.1",
     author="Semantic Vectors Authors",
     author_email="semanticvectors@googlegroups.com",
     description="Semantic Vectors work in Python",
