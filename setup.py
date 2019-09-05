1b3cb1783076470779bd0849f3e089ed2fe30ce0
Dominic Widdows <dominic.widdows@grab.com>
2019-09-05 10:25:59 -0700

    Changing format strings to be Python 3.5 compliant

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="semvecpy",
-    version="0.1.4",
+    version="0.1.5",
     author="Semantic Vectors Authors",
     author_email="semanticvectors@googlegroups.com",
     description="Semantic Vectors work in Python",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/semanticvector/semvecpy",
     packages=setuptools.find_packages(),
-    python_requires='>=3',
+    python_requires=">=3",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
