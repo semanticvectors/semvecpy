9970c7d9c3d109f250e7ce211ab38eca788498de
treversec <treversec@gmail.com>
2019-09-20 13:22:07 -0700

    Scale initialization of random vectors

--- semvecpy/vectors/real_vectors.py
+++ semvecpy/vectors/real_vectors.py
@@ -45,7 +45,7 @@ class RealVectorFactory:
 
     @staticmethod
     def generate_random_vector(dimension):
-        randvec = vu.create_dense_random_vector(dimension, None)
+        randvec = np.random.uniform(low=-.5*dimension, high=.5*dimension, size=dimension)
         return RealVectorFactory.generate_vector(randvec)
 
     @staticmethod
