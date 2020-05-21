3ccdc3de590ed2efc9321dc3f341cde441b7ca0e
treversec <treversec@gmail.com>
2020-05-20 17:42:16 -0700

    Correct weight decrement when logging up

--- semvecpy/vectors/binary_bitvectors.py
+++ semvecpy/vectors/binary_bitvectors.py
@@ -297,7 +297,7 @@ class BinaryVector(object):
         # e.g. if adding with a weight of eight, start in the third row
         rowfloor = int(np.floor(np.log2(weight))) - 1
         while 0 < rowfloor < len(self.pvr):
-            weight = weight - int(np.power(2, rowfloor))
+            weight = weight - int(np.power(2, (rowfloor+1))
             self.addfromfloor(self.cv, self.pvr, rowfloor)
             self.cv = other.bitset.copy()
             self.addfromfloor(self.cv, self.nvr, rowfloor)
