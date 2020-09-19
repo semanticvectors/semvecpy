Fixed bug involving incorrect calculation of length of utf-8 encoding in the case of strings with special characters.

semvecpy/permutations/dense_permutations.py
@@ -3,7 +3,7 @@
 There are some overlaps and some differences with sparse_permutations.py, we haven't attempted to unify these.
 """
 import numpy as np
-
+import random
 from semvecpy.permutations import constants as c
 from semvecpy.vectors.vector_utils import normalize, cosine_similarity
 
@@ -55,6 +55,18 @@ def inverse_permutation(permutation):
     """
     return np.argsort(permutation)
 
+
+def swap_permutation(permutation, numswaps):
+    """
+    Swap numswaps pairs of integers in the permutation
+    Input permutation should be an index vector (e.g. a permutation of
+    the integers from 0 to dimension-1 of the vector).
+    """
+    newperm = permutation.copy()
+    for x in range(numswaps):
+        a, b = random.sample(range(len(permutation)), 2)
+        newperm[a],newperm[b] = newperm[b],newperm[a]
+    return newperm
 
 def permutation_to_matrix(permutation):
     """

semvecpy/vectors/semvec_utils.py
@@ -304,11 +304,11 @@ def write_realvectors(vecstore, filename):
     with open(filename, mode='wb') as file:  # b is important -> binary
         x = '-vectortype REAL -dimension '+str(np.asarray(vecstore.vectors).shape[1])
         file.write((len(x)).to_bytes(1,byteorder='little', signed=False))
-        file.write(x.encode())
+        file.write(x.encode('utf-8'))
         for word in vecstore.dict.keys():
-            vint = get_vint(len(word))
+            vint = get_vint(len(word.encode('utf-8')))
             file.write(vint)
-            file.write(word.encode())
+            file.write(word.encode('utf-8'))
             floats = vecstore.get_vector(word).vector
             s = struct.pack('>'+str(len(floats))+'f', *floats)
             file.write(s)
@@ -320,11 +320,11 @@ def write_bitarray_binaryvectors(vecstore, filename):
     with open(filename, mode='wb') as file:  # b is important -> binary
         x = '-vectortype BINARY -dimension ' + str(vecstore.vectors[0].dimension)
         file.write((len(x)).to_bytes(1, byteorder='little', signed=False))
-        file.write(x.encode())
+        file.write(x.encode('utf-8'))
         for word in vecstore.dict.keys():
-            vint = get_vint(len(word))
+            vint = get_vint(len(word.encode('utf-8')))
             file.write(vint)
-            file.write(word.encode())
+            file.write(word.encode('utf-8'))
             bins = vecstore.get_vector(word).bitset
             file.write(bins)
 
@@ -336,10 +336,10 @@ def write_packed_binaryvectors(vecstore, filename):
     with open(filename, mode='wb') as file:  # b is important -> binary
         x = '-vectortype BINARY -dimension ' + str(vecstore.vectors[0].dimension)
         file.write((len(x)).to_bytes(1, byteorder='little', signed=False))
-        file.write(x.encode())
+        file.write(x.encode('utf-8'))
         for word in vecstore.dict.keys():
-            vint = get_vint(len(word))
+            vint = get_vint(len(word.encode('utf-8')))
             file.write(vint)
-            file.write(word.encode())
+            file.write(word.encode('utf-8'))
             bins = vecstore.get_vector(word).bitset
             file.write(bins)